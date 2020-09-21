c4cddbf5d3d8f60432896aaf06f4bd678b0256ad
tabularasa066a <tabularasa066a@gmail.com>
2020-09-22 07:05:51 +0900

    a

---
+++ EfficientGAN_PyTorch/efficient_gan.py
@@ -0,0 +1,560 @@
+# パッケージのimport
+import random
+import math
+import time
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.utils.data as data
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torchvision import transforms
+
+
+# Setup seeds
+torch.manual_seed(1234)
+torch.cuda.manual_seed(1234)
+np.random.seed(1234)
+random.seed(1234)
+
+
+class Generator(nn.Module):
+
+    def __init__(self, z_dim=20):
+        super(Generator, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Linear(z_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(inplace=True))
+
+        self.layer2 = nn.Sequential(
+            nn.Linear(1024, 7*7*128),
+            nn.BatchNorm1d(7*7*128),
+            nn.ReLU(inplace=True))
+
+        self.layer3 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=128, out_channels=64,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True))
+
+        self.last = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=64, out_channels=1,
+                               kernel_size=4, stride=2, padding=1),
+            nn.Tanh())
+        # 注意：白黒画像なので出力チャネルは1つだけ
+
+    def forward(self, z):
+        out = self.layer1(z)
+        out = self.layer2(out)
+
+        # 転置畳み込み層に入れるためにテンソルの形を整形
+        out = out.view(z.shape[0], 128, 7, 7)
+        out = self.layer3(out)
+        out = self.last(out)
+
+        return out
+
+
+# 動作確認
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+G = Generator(z_dim=20)
+G.train()
+
+# 入力する乱数
+# バッチノーマライゼーションがあるのでミニバッチ数は2以上
+input_z = torch.randn(2, 20)
+
+# 偽画像を出力
+fake_images = G(input_z)  # torch.Size([2, 1, 28, 28])
+img_transformed = fake_images[0][0].detach().numpy()
+plt.imshow(img_transformed, 'gray')
+plt.show()
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, z_dim=20):
+        super(Discriminator, self).__init__()
+
+        # 画像側の入力処理
+        self.x_layer1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True))
+        # 注意：白黒画像なので入力チャネルは1つだけ
+
+        self.x_layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=4,
+                      stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        # 乱数側の入力処理
+        self.z_layer1 = nn.Linear(z_dim, 512)
+
+        # 最後の判定
+        self.last1 = nn.Sequential(
+            nn.Linear(3648, 1024),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        self.last2 = nn.Linear(1024, 1)
+
+    def forward(self, x, z):
+
+        # 画像側の入力処理
+        x_out = self.x_layer1(x)
+        x_out = self.x_layer2(x_out)
+
+        # 乱数側の入力処理
+        z = z.view(z.shape[0], -1)
+        z_out = self.z_layer1(z)
+
+        # x_outとz_outを結合し、全結合層で判定
+        x_out = x_out.view(-1, 64 * 7 * 7)
+        out = torch.cat([x_out, z_out], dim=1)
+        out = self.last1(out)
+
+        feature = out  # 最後にチャネルを1つに集約する手前の情報
+        feature = feature.view(feature.size()[0], -1)  # 2次元に変換
+
+        out = self.last2(out)
+
+        return out, feature
+
+
+# 動作確認
+D = Discriminator(z_dim=20)
+
+# 偽画像を生成
+input_z = torch.randn(2, 20)
+fake_images = G(input_z)
+
+# 偽画像をDに入力
+d_out, _ = D(fake_images, input_z)
+
+# 出力d_outにSigmoidをかけて0から1に変換
+print(nn.Sigmoid()(d_out))
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, z_dim=20):
+        super(Encoder, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3,
+                      stride=1),
+            nn.LeakyReLU(0.1, inplace=True))
+        # 注意：白黒画像なので入力チャネルは1つだけ
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3,
+                      stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3,
+                      stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        # ここまでで画像のサイズは7×7になっている
+        self.last = nn.Linear(128 * 7 * 7, z_dim)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+
+        # FCに入れるためにテンソルの形を整形
+        out = out.view(-1, 128 * 7 * 7)
+        out = self.last(out)
+
+        return out
+
+
+# 動作確認
+E = Encoder(z_dim=20)
+
+# 入力する画像データ
+x = fake_images  # fake_imagesは上のGで作成したもの
+
+# 画像からzをEncode
+z = E(x)
+
+print(z.shape)
+print(z)
+
+
+def make_datapath_list():
+    """学習、検証の画像データとアノテーションデータへのファイルパスリストを作成する。 """
+
+    train_img_list = list()  # 画像ファイルパスを格納
+
+    for img_idx in range(200):
+        img_path = "./data/img_78_28size/img_7_" + str(img_idx)+'.jpg'
+        train_img_list.append(img_path)
+
+        img_path = "./data/img_78_28size/img_8_" + str(img_idx)+'.jpg'
+        train_img_list.append(img_path)
+
+    return train_img_list
+
+
+class ImageTransform():
+    """画像の前処理クラス"""
+
+    def __init__(self, mean, std):
+        self.data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
+    def __call__(self, img):
+        return self.data_transform(img)
+
+
+class GAN_Img_Dataset(data.Dataset):
+    """画像のDatasetクラス。PyTorchのDatasetクラスを継承"""
+
+    def __init__(self, file_list, transform):
+        self.file_list = file_list
+        self.transform = transform
+
+    def __len__(self):
+        '''画像の枚数を返す'''
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        '''前処理をした画像のTensor形式のデータを取得'''
+
+        img_path = self.file_list[index]
+        img = Image.open(img_path)  # [高さ][幅]白黒
+
+        # 画像の前処理
+        img_transformed = self.transform(img)
+
+        return img_transformed
+
+
+# DataLoaderの作成と動作確認
+
+# ファイルリストを作成
+train_img_list=make_datapath_list()
+
+# Datasetを作成
+mean = (0.5,)
+std = (0.5,)
+train_dataset = GAN_Img_Dataset(
+    file_list=train_img_list, transform=ImageTransform(mean, std))
+
+# DataLoaderを作成
+batch_size = 64
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
+
+# 動作の確認
+batch_iterator = iter(train_dataloader)  # イテレータに変換
+imges = next(batch_iterator)  # 1番目の要素を取り出す
+print(imges.size())  # torch.Size([64, 1, 64, 64])
+
+
+# モデルを学習させる関数を作成
+
+
+def train_model(G, D, E, dataloader, num_epochs):
+
+    # GPUが使えるかを確認
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("使用デバイス：", device)
+
+    # 最適化手法の設定
+    lr_ge = 0.0001
+    lr_d = 0.0001/4
+    beta1, beta2 = 0.5, 0.999
+    g_optimizer = torch.optim.Adam(G.parameters(), lr_ge, [beta1, beta2])
+    e_optimizer = torch.optim.Adam(E.parameters(), lr_ge, [beta1, beta2])
+    d_optimizer = torch.optim.Adam(D.parameters(), lr_d, [beta1, beta2])
+
+    # 誤差関数を定義
+    # BCEWithLogitsLossは入力にシグモイド（logit）をかけてから、
+    # バイナリークロスエントロピーを計算
+    criterion = nn.BCEWithLogitsLoss(reduction='mean')
+
+    # パラメータをハードコーディング
+    z_dim = 20
+    mini_batch_size = 64
+
+    # ネットワークをGPUへ
+    G.to(device)
+    E.to(device)
+    D.to(device)
+
+    G.train()  # モデルを訓練モードに
+    E.train()  # モデルを訓練モードに
+    D.train()  # モデルを訓練モードに
+
+    # ネットワークがある程度固定であれば、高速化させる
+    torch.backends.cudnn.benchmark = True
+
+    # 画像の枚数
+    num_train_imgs = len(dataloader.dataset)
+    batch_size = dataloader.batch_size
+
+    # イテレーションカウンタをセット
+    iteration = 1
+    logs = []
+
+    # epochのループ
+    for epoch in range(num_epochs):
+
+        # 開始時刻を保存
+        t_epoch_start = time.time()
+        epoch_g_loss = 0.0  # epochの損失和
+        epoch_e_loss = 0.0  # epochの損失和
+        epoch_d_loss = 0.0  # epochの損失和
+
+        print('-------------')
+        print('Epoch {}/{}'.format(epoch, num_epochs))
+        print('-------------')
+        print('（train）')
+
+        # データローダーからminibatchずつ取り出すループ
+        for imges in dataloader:
+
+            # ミニバッチがサイズが1だと、バッチノーマライゼーションでエラーになるのでさける
+            if imges.size()[0] == 1:
+                continue
+
+            # ミニバッチサイズの1もしくは0のラベル役のテンソルを作成
+            # 正解ラベルと偽ラベルを作成
+            # epochの最後のイテレーションはミニバッチの数が少なくなる
+            mini_batch_size = imges.size()[0]
+            label_real = torch.full((mini_batch_size,), fill_value=1, dtype=torch.float64).to(device)  # 1で埋めてfloat64に変換
+            label_fake = torch.full((mini_batch_size,), fill_value=0, dtype=torch.float64).to(device)
+
+            # GPUが使えるならGPUにデータを送る
+            imges = imges.to(device)
+
+            # --------------------
+            # 1. Discriminatorの学習
+            # --------------------
+            # 真の画像を判定　
+            z_out_real = E(imges)
+            d_out_real, _ = D(imges, z_out_real)
+
+            # 偽の画像を生成して判定
+            input_z = torch.randn(mini_batch_size, z_dim).to(device)
+            fake_images = G(input_z)
+            d_out_fake, _ = D(fake_images, input_z)
+
+            # 誤差を計算
+            d_loss_real = criterion(d_out_real.view(-1), label_real)
+            d_loss_fake = criterion(d_out_fake.view(-1), label_fake)
+            d_loss = d_loss_real + d_loss_fake
+
+            # バックプロパゲーション
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            # --------------------
+            # 2. Generatorの学習
+            # --------------------
+            # 偽の画像を生成して判定
+            input_z = torch.randn(mini_batch_size, z_dim).to(device)
+            fake_images = G(input_z)
+            d_out_fake, _ = D(fake_images, input_z)
+
+            # 誤差を計算
+            g_loss = criterion(d_out_fake.view(-1), label_real)
+
+            # バックプロパゲーション
+            g_optimizer.zero_grad()
+            g_loss.backward()
+            g_optimizer.step()
+
+            # --------------------
+            # 3. Encoderの学習
+            # --------------------
+            # 真の画像のzを推定
+            z_out_real = E(imges)
+            d_out_real, _ = D(imges, z_out_real)
+
+            # 誤差を計算
+            e_loss = criterion(d_out_real.view(-1), label_fake)
+
+            # バックプロパゲーション
+            e_optimizer.zero_grad()
+            e_loss.backward()
+            e_optimizer.step()
+
+            # --------------------
+            # 4. 記録
+            # --------------------
+            epoch_d_loss += d_loss.item()
+            epoch_g_loss += g_loss.item()
+            epoch_e_loss += e_loss.item()
+            iteration += 1
+
+        # epochのphaseごとのlossと正解率
+        t_epoch_finish = time.time()
+        print('-------------')
+        print('epoch {} || Epoch_D_Loss:{:.4f} ||Epoch_G_Loss:{:.4f} ||Epoch_E_Loss:{:.4f}'.format(
+            epoch, epoch_d_loss/batch_size, epoch_g_loss/batch_size, epoch_e_loss/batch_size))
+        print('timer:  {:.4f} sec.'.format(t_epoch_finish - t_epoch_start))
+        t_epoch_start = time.time()
+
+    print("総イテレーション回数:", iteration)
+
+    return G, D, E
+
+
+# ネットワークの初期化
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        # conv2dとConvTranspose2dの初期化
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+    elif classname.find('BatchNorm') != -1:
+        # BatchNorm2dの初期化
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+    elif classname.find('Linear') != -1:
+        # 全結合層Linearの初期化
+        m.bias.data.fill_(0)
+
+
+# 初期化の実施
+G.apply(weights_init)
+E.apply(weights_init)
+D.apply(weights_init)
+
+print("ネットワークの初期化完了")
+
+
+
+# 学習・検証を実行する
+# 15分ほどかかる
+num_epochs = 1500
+G_update, D_update, E_update = train_model(
+    G, D, E, dataloader=train_dataloader, num_epochs=num_epochs)
+
+
+# テスト用のDataLoaderの作成
+
+
+def make_test_datapath_list():
+    """学習、検証の画像データとアノテーションデータへのファイルパスリストを作成する。 """
+
+    train_img_list = list()  # 画像ファイルパスを格納
+
+    for img_idx in range(5):
+        img_path = "./data/test_28size/img_7_" + str(img_idx)+'.jpg'
+        train_img_list.append(img_path)
+
+        img_path = "./data/test_28size/img_8_" + str(img_idx)+'.jpg'
+        train_img_list.append(img_path)
+
+        img_path = "./data/test_28size/img_2_" + str(img_idx)+'.jpg'
+        train_img_list.append(img_path)
+
+    return train_img_list
+
+
+# ファイルリストを作成
+test_img_list = make_test_datapath_list()
+
+# Datasetを作成
+mean = (0.5,)
+std = (0.5,)
+test_dataset = GAN_Img_Dataset(
+    file_list=test_img_list, transform=ImageTransform(mean, std))
+
+# DataLoaderを作成
+batch_size = 5
+
+test_dataloader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False)
+
+
+# テストデータの確認
+batch_iterator = iter(test_dataloader)  # イテレータに変換
+imges = next(batch_iterator)  # 1番目の要素を取り出す
+
+fig = plt.figure(figsize=(15, 6))
+for i in range(0, 5):
+    # 上段に訓練データを
+    plt.subplot(2, 5, i+1)
+    plt.imshow(imges[i][0].cpu().detach().numpy(), 'gray')
+
+
+def Anomaly_score(x, fake_img, z_out_real, D, Lambda=0.1):
+
+    # テスト画像xと生成画像fake_imgのピクセルレベルの差の絶対値を求めて、ミニバッチごとに和を求める
+    residual_loss = torch.abs(x-fake_img)
+    residual_loss = residual_loss.view(residual_loss.size()[0], -1)
+    residual_loss = torch.sum(residual_loss, dim=1)
+
+    # テスト画像xと生成画像fake_imgを識別器Dに入力し、特徴量マップを取り出す
+
+    _, x_feature = D(x, z_out_real)
+    _, G_feature = D(fake_img, z_out_real)
+
+    # テスト画像xと生成画像fake_imgの特徴量の差の絶対値を求めて、ミニバッチごとに和を求める
+    discrimination_loss = torch.abs(x_feature-G_feature)
+    discrimination_loss = discrimination_loss.view(
+        discrimination_loss.size()[0], -1)
+    discrimination_loss = torch.sum(discrimination_loss, dim=1)
+
+    # ミニバッチごとに2種類の損失を足し算する
+    loss_each = (1-Lambda)*residual_loss + Lambda*discrimination_loss
+
+    # ミニバッチ全部の損失を求める
+    total_loss = torch.sum(loss_each)
+
+    return total_loss, loss_each, residual_loss
+
+
+# 異常検知したい画像
+x = imges[0:5]
+x = x.to(device)
+
+# 教師データの画像をエンコードしてzにしてから、Gで生成
+E_update.eval()
+G_update.eval()
+z_out_real = E_update(imges.to(device))
+imges_reconstract = G_update(z_out_real)
+
+# 損失を求める
+loss, loss_each, residual_loss_each = Anomaly_score(
+    x, imges_reconstract, z_out_real, D_update, Lambda=0.1)
+
+# 損失の計算。トータルの損失
+loss_each = loss_each.cpu().detach().numpy()
+print("total loss：", np.round(loss_each, 0))
+
+# 画像を可視化
+fig = plt.figure(figsize=(15, 6))
+for i in range(0, 5):
+    # 上段に訓練データを
+    plt.subplot(2, 5, i+1)
+    plt.imshow(imges[i][0].cpu().detach().numpy(), 'gray')
+
+    # 下段に生成データを表示する
+    plt.subplot(2, 5, 5+i+1)
+    plt.imshow(imges_reconstract[i][0].cpu().detach().numpy(), 'gray')
+
+
+
